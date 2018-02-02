c58db77aff0905d6bdafdf9f065250ff727d2a2a
susumOyaji <sumitomo0210@gmail.com>
2018-02-02 21:39:48 +0900

    win7 to push

--- ListView/ListViewPageViewModel.cs
+++ ListView/ListViewPageViewModel.cs
@@ -11,7 +11,7 @@ namespace ListView
     /// <summary>
     /// ListView を表示する View の ViewModel
     /// </summary>
-    class ListViewPageViewModel : INotifyPropertyChanged
+    class ListViewPageViewModel : ViewModelBase //INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -23,7 +23,9 @@ namespace ListView
         /// <summary>
         /// ListView へ表示するデータ
         /// </summary>
-        //public ObservableCollection<string> ItemList { get; } = new ObservableCollection<string>(new[] { "item01", "item02", "item03", "item04" });
+        //ObservableCollection<string> ItemList { get; } = new ObservableCollection<string>(new[] { "item01", "item02", "item03", });
+
+        
 
 
 
@@ -31,7 +33,80 @@ namespace ListView
         /// ListView の各 Item 内の Button にバインディングする Command
         /// </summary>
         public ICommand ItemCommand { get; }
-        List<Price> price = new List<Price>();
+        //List<Price> price = new List<Price>();
+        public ObservableCollection<Price> ItemList { get; set; }
+
+
+        private static decimal _payAssetprice;
+        public decimal PayAssetprice//保有数* 購入価格 = 投資総額
+        {
+            get { return _payAssetprice; }
+            set
+            {
+                _payAssetprice = value;
+                this.OnPropertyChanged(nameof(PayAssetprice));
+            }
+        }
+
+        public static decimal _totalAsset;
+        public decimal TotalAsset //現在評価額合計
+        {
+            get { return _totalAsset; }
+            set
+            {
+                _totalAsset = value;
+               this.OnPropertyChanged(nameof(TotalAsset));
+            }
+
+        }
+
+        private static decimal _currentStockPrice;
+        public decimal NewYorkStockPrice
+        {
+            get { return _currentStockPrice; }
+            set
+            {
+                _currentStockPrice = value;
+                this.OnPropertyChanged(nameof(NewYorkStockPrice));
+            }
+        }
+
+
+
+        private static string _currentStockPercent;
+        public string NewYorkStockPercent
+        {
+            get { return _currentStockPercent; }
+            set
+            {
+                _currentStockPercent = value;
+                this.OnPropertyChanged(nameof(NewYorkStockPercent));
+            }
+        }
+
+        private static string _polar;
+        public string Polar//(-)下落
+        {
+            get { return _polar; }
+            set
+            {
+                _polar = value;
+                // StockMvvmView.ChangeButtonColor(_polar);
+                this.OnPropertyChanged(nameof(Polar));
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
 
         /// <summary>
         /// デフォルト コンストラクタ
@@ -39,9 +114,60 @@ namespace ListView
         public ListViewPageViewModel()
         {
             ItemCommand = new CountUpCommand(OnItemCommand);
+
+            ItemList = new ObservableCollection<Price>();
+
             Sample();
             DispSet();
         }
+
+
+
+
+        #region メソッド
+
+        private async void NewYorkStock()
+        {
+            //CurrentColor = MainModelS.NewyorkButtonColer(CurrentColor);
+            var anser = await Models.Models.Getserchi("^DJI");
+            NewYorkStockPrice = anser.Realprice;
+            NewYorkStockPercent = anser.Prev_day;
+
+            if (anser.Polar == "Green")
+            {
+                //NewYorkStockPercent = anser.Percent;
+                Polar = anser.Polar;// "Green";
+            }
+            if (anser.Polar == "Red")
+            {
+                //NewYorkStockPercent = Dayratio;
+                Polar = anser.Polar; //CurrentColor = "Red";
+            }
+
+        }
+
+        private async void TokyoStock()
+        {
+            var anser = await Models.Models.Getserchi("998407");
+            TokyoStockPrice = anser.Realprice;
+            TokyoStockPercent = anser.Prev_day;
+
+            if (anser.Polar == "-")
+            {
+                //NewYorkStockPercent = anser.Percent;
+                CurrentColor = "Green";
+            }
+            if (anser.Polar == "+")
+            {
+                //NewYorkStockPercent = Dayratio;
+                CurrentColor = "Red";
+            }
+
+        }
+
+        #endregion
+
+
 
         /// <summary>
         /// ListView の Button 押下時の動作
@@ -50,65 +176,31 @@ namespace ListView
         private void OnItemCommand()
         {
             View.DisplayAlert("XSample", ItemCommand.ToString(), "OK");
-            View.ChangeButtonColor();
+           // View.ChangeButtonColor();
         }
 
 
      
-
-
-
-
-
-
-
-
-        public ObservableCollection<Price> ItemList { get; set; }
-       
+               
         public void Sample()
         {
-            //PriceItemList = new ObservableCollection<Price>();
+            ItemList = new ObservableCollection<Price>();
 
 
-            ItemList = new ObservableCollection<Price>() {
-                new Price()
-                {
-                    Name = "Sony",
-                    Stocks = 100,
-                    Itemprice = 2015,
-                    Realprice = 1000,
-                    RealValue = 100,
-                    Percent = "5"
-                },
-                new Price()
-                {
-                    Name = "Morito",
-                    Stocks = 100,
-                    Itemprice = 2015,
-                    Realprice = 1000,
-                    RealValue = 100,
-                    Percent = "5"
-                },
-
-
-            };
+            ItemList.Add(new Price
+            {
+                Name = "SampleSony",
+                Stocks = 100,
+                Itemprice = 2015,
+                Realprice = 1000,
+                RealValue = 100,
+                Percent = "5"
+            });
 
 
         }
 
      
-
-        private decimal _stocks { get; set; }
-        public decimal Stocks
-       {
-            get { return _stocks; }
-            set
-            {
-                _stocks = value;
-               // this.OnPropertyChanged(nameof(Stocks));
-            }
-
-        }
 
 
 
@@ -120,35 +212,37 @@ namespace ListView
             decimal TotalAssetAdd = 0;
             decimal PayAssetpriceAdd = 0;
 
-
-
-
             // UTF8のファイルの書き込み Edit. 
             string write = await StorageControl.PCLSaveCommand("6758,200,1665\n9837,200,712\n6976,200,1846");//登録データ書き込み
-            List<Price> prices = Finance.Parse(await StorageControl.PCLLoadCommand());//登録データ読み込み
+           // List<Price> prices = Finance.Parse(await StorageControl.PCLLoadCommand());//登録データ読み込み
             List<Price> pricesanser = await Models.PasonalGetserchi();//登録データの現在値を取得する
 
+            
 
-
-
-
-            foreach (Price item in prices)
+            foreach (Price item in pricesanser)
             {
-                Stocks = pricesanser[i].Stocks;//保有数*
-                //Itemprice = pricesanser[i].Itemprice;//購入価格*
-                //Realprice = pricesanser[i].Realprice;//現在値*
-                //Percent = pricesanser[i].Percent;//前日比％**
+
+                ItemList.Add(new Price
+                {
+                    Name = item.Name,// "Sony",
+                    Stocks = item.Stocks,//保有数*
+                    Itemprice = item.Itemprice,// 2015,
+                    Realprice = item.Realprice,//現在値*// 1000,
+                    RealValue = item.RealValue,// 100,
+                    Percent = item.Percent//前日比％**// "5"
+                });
+
                 PayAssetpriceAdd = PayAssetpriceAdd + (pricesanser[i].Stocks * Convert.ToDecimal(pricesanser[i].Itemprice));//株数*購入単価の合計
 
                 TotalAssetAdd = TotalAssetAdd + (pricesanser[i].Stocks * Convert.ToDecimal(pricesanser[i].Realprice));//現在評価額
                 //Polar = pricesanser[i].Polar;
 
-                ItemList.Add(pricesanser[i]);// item);
+               // ItemList.Add(item);// item);
 
                 i = ++i;
             }
-            //PayAssetprice = PayAssetpriceAdd;
-            //TotalAsset = TotalAssetAdd;
+            PayAssetprice = PayAssetpriceAdd;
+            TotalAsset = TotalAssetAdd;
 
 
 
